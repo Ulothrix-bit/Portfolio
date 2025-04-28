8ca47a1feb0e59d57845c29dc66f9441bdd74bc2
Sourabh Yadav <sourabhyadav643@gmail.com>
2025-04-28 20:49:52 +0530

    updated github link and improved skill section

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const video1= document.getElementById('projectVideo1');
-const video2= document.getElementById('projectVideo2');
-const video3= document.getElementById('projectVideo3');
-const videoList =[video1,video2,video3];
-videoList.forEach(function(video){
-    video.addEventListener('mouseover',function(){
+const video1 = document.getElementById('projectVideo1');
+const video2 = document.getElementById('projectVideo2');
+const video3 = document.getElementById('projectVideo3');
+const videoList = [video1, video2, video3];
+videoList.forEach(function (video) {
+    video.addEventListener('mouseover', function () {
         video.play()
     })
-    video.addEventListener('mouseout',function(){
+    video.addEventListener('mouseout', function () {
         video.pause()
     })
 })
